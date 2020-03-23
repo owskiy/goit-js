8c21e87f364f26669e1d9059cbfd5f644abb98e0
owskiy <Sabakabarabaka18@gmail.com>
2020-03-23 15:22:15 +0200

    task5isdone

--- homework-1/task4.js
+++ homework-1/task4.js
@@ -1,27 +1,27 @@
-"use strict";
+// "use strict";
 
-const total = 100;
-let ordered = 150;
+// const total = 100;
+// let ordered = 150;
 
-if (ordered > total) {
-    console.log("На складе недостаточно товаров!");
-} else {
-    console.log("Заказ оформлен, с вами свяжется менеджер");
-}
+// if (ordered > total) {
+//     console.log("На складе недостаточно товаров!");
+// } else {
+//     console.log("Заказ оформлен, с вами свяжется менеджер");
+// }
 
 
-ordered = 100;
+// ordered = 100;
 
-if (ordered > total) {
-    console.log("На складе недостаточно товаров!");
-} else {
-    console.log("Заказ оформлен, с вами свяжется менеджер");
-}
+// if (ordered > total) {
+//     console.log("На складе недостаточно товаров!");
+// } else {
+//     console.log("Заказ оформлен, с вами свяжется менеджер");
+// }
 
-ordered = 60;
+// ordered = 60;
 
-if (ordered > total) {
-    console.log("На складе недостаточно товаров!");
-} else {
-    console.log("Заказ оформлен, с вами свяжется менеджер");
-}
+// if (ordered > total) {
+//     console.log("На складе недостаточно товаров!");
+// } else {
+//     console.log("Заказ оформлен, с вами свяжется менеджер");
+// }
---
+++ homework-1/task5.js
@@ -0,0 +1,16 @@
+// "use strict";
+
+// const ADMIN_PASSWORD = 'jqueryismyjam';
+
+// let message;
+
+// let entered_password = prompt("Enter your password")
+
+// if (entered_password == null) {
+//     message = 'Отменено пользоватeлем!';
+// } else if (entered_password === ADMIN_PASSWORD) {
+//     message = ('Добро пожаловать!');
+// } else {
+//     message = ('Доступ запрещен, неверный пароль!');
+// }
+// alert(message);
