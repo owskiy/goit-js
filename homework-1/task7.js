a99f68fc51fb5f433e9c12976ca6d0f5addffb5e
owskiy <Sabakabarabaka18@gmail.com>
2020-03-23 16:02:38 +0200

    task7isdone

---
+++ homework-1/task7.js
@@ -0,0 +1,37 @@
+"use strict";
+
+let country;
+country = prompt("Введите страну").toUpperCase();
+
+let countryPay;
+
+    switch (country) {
+    case "китай":
+        countryPay = 100;
+    alert(`Доставка в ${country} будет стоить ${countryPay} кредитов`);
+    break;
+    
+    case "чили":
+        countryPay = 250;
+        alert(`Доставка в ${country} будет стоить ${countryPay} кредитов`);
+    break;
+
+    case "австралия":
+        countryPay = 170;
+        alert(`Доставка в ${country} будет стоить ${countryPay} кредитов`);
+    break;
+
+    case "индия":
+        countryPay = 180;
+        alert(`Доставка в ${country} будет стоить ${countryPay} кредитов`);
+    break;
+
+    case "ямайка":
+        countryPay = 120;
+        alert(`Доставка в ${country} будет стоить ${countryPay} кредитов`);
+    break;
+
+    default:
+        alert(`В вашей стране ${country} доставка не доступна`);
+    
+}
